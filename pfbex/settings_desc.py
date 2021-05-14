7956708a0eb0777d928cd06e412d089e298ba8e6
Andreas Schroeder <andreas@a-netz.de>
2021-05-14 08:29:04 +0200

    Reorder configuration structure

--- pfbex/settings_desc.py
+++ pfbex/settings_desc.py
@@ -1,28 +1,9 @@
 '''Settings descriptor'''
 
 SETTINGS_DESC = {
-    'HTTP_PORT': {
-        'default': 8765,
-        'help': 'Port for the integrated http web server'
-    },
-    'LOGLEVEL': {
-        'default': 'info',
-        'help': "Log level, one of 'debug', 'info', 'warning' or 'error'"
-    },
-    'METRICS_PATH': {
-        'default': './conf',
-        'help': 'Path to the metrics configuration files.'
-    },
-    'DUMP_SERVICES': {
-        'default': 0,
-        'help': 'Set to 1 to dump service information of the FritzBox at startup.'
-    },
-    'DUMP_DATA': {
-        'default': '0',
-        'help': (
-            'Set to 1 to dump data with services. This might dump sensitive'
-            'information.')
-    },
+
+    # Basic connection settings
+
     'FRITZ_HOST': {
         'default': 'fritz.box',
         'help': 'Hostname of the FritzBox to query.'
@@ -35,10 +16,38 @@ SETTINGS_DESC = {
         'required': True,
         'help': 'Password to log in to the FritzBox to retrieve metrics'
     },
+
+    # Internal settings
+
+    'HTTP_PORT': {
+        'default': 8765,
+        'help': 'Port for the integrated http web server'
+    },
+    'LOGLEVEL': {
+        'default': 'info',
+        'help': "Log level, one of 'debug', 'info', 'warning' or 'error'"
+    },
+    'METRICS_PATH': {
+        'default': './conf',
+        'help': 'Path to the metrics configuration files.'
+    },
     'CACHE_TIME': {
         'default': 30,
         'help': (
             'Time in seconds to keep results in the internal cache before '
             'querying the FritzBox again')
+    },
+
+    # Data dump settings
+
+    'DUMP_SERVICES': {
+        'default': 0,
+        'help': 'Set to 1 to dump service information of the FritzBox at startup.'
+    },
+    'DUMP_DATA': {
+        'default': '0',
+        'help': (
+            'Set to 1 to dump data with services. This might dump sensitive'
+            'information.')
     },
 }
